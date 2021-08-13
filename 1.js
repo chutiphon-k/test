f06a21125386a50e6ccc3c59cfcc1316b23840f1
Chutiphon K <chutiphon.ksp@gmail.com>
2021-08-13 23:54:41 +0700

    commit

--- 1.js
+++ 1.js
@@ -1,28 +1,28 @@
-const input = [ 'AMOR', 'XISELA', 'JAMON', 'ROMA', 'OMAR', 'MORA', 'ESPONJA', 'RAMO', 'JAPONES', 'ARMO', 'MOJAN', 'MARO', 'ORAM', 'MONJA', 'ALEXIS' ];
+// const input = [ 'AMOR', 'XISELA', 'JAMON', 'ROMA', 'OMAR', 'MORA', 'ESPONJA', 'RAMO', 'JAPONES', 'ARMO', 'MOJAN', 'MARO', 'ORAM', 'MONJA', 'ALEXIS' ];
 
-const groups = {
-  a: [ 'AMOR', 'ROMA', 'OMAR', 'MORA', 'RAMO', 'ARMO', 'MARO', 'ORAM'],
-  b: [ 'XISELA', 'ALEXIS'],
-  c: [ 'JAMON', 'MOJAN', 'MONJA'],
-  d: [ 'ESPONJA', 'JAPONES'],
-}
+// const groups = {
+//   a: [ 'AMOR', 'ROMA', 'OMAR', 'MORA', 'RAMO', 'ARMO', 'MARO', 'ORAM'],
+//   b: [ 'XISELA', 'ALEXIS'],
+//   c: [ 'JAMON', 'MOJAN', 'MONJA'],
+//   d: [ 'ESPONJA', 'JAPONES'],
+// }
 
-const temp = {};
+// const temp = {};
 
-input.map((value) => {
-  Object.keys(groups).map((groupName) => {
-    if (groups[groupName].includes(value)) {
-      if (Array.isArray(temp[groupName])) {
-        temp[groupName].push(value);
-      } else {
-        temp[groupName] = [value];
-      }
-    }
-  })
-})
+// input.map((value) => {
+//   Object.keys(groups).map((groupName) => {
+//     if (groups[groupName].includes(value)) {
+//       if (Array.isArray(temp[groupName])) {
+//         temp[groupName].push(value);
+//       } else {
+//         temp[groupName] = [value];
+//       }
+//     }
+//   })
+// })
 
-const output = Object.values(temp);
+// const output = Object.values(temp);
 
-console.log('------------------------------------');
-console.log(output);
-console.log('------------------------------------');
+// console.log('------------------------------------');
+// console.log(output);
+// console.log('------------------------------------');
